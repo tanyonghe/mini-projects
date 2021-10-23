61998b1b4002621f404b5100609f21f8da042090
unknown <tanyonghe@u.nus.edu>
2021-10-23 16:12:24 -0700

    Add sprite movement and direction

--- games/platformer/main.py
+++ games/platformer/main.py
@@ -3,9 +3,14 @@ from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+FPS = 60
+
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 TILE_SIZE = 50
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 80
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Platformer')
@@ -18,6 +23,88 @@ def draw_grid():
 	for line in range(0, 20):
 		pygame.draw.line(screen, (255, 255, 255), (0, line * TILE_SIZE), (SCREEN_WIDTH, line * TILE_SIZE))
 		pygame.draw.line(screen, (255, 255, 255), (line * TILE_SIZE, 0), (line * TILE_SIZE, SCREEN_HEIGHT))
+
+class Player():
+	def __init__(self, x, y):
+		self.images_right = []
+		self.images_left = []
+		self.index = 0
+		self.counter = 0
+		for i in range(1, 5):
+			img_right = pygame.image.load(f'img/guy{i}.png')
+			img_right = pygame.transform.scale(img_right, (PLAYER_WIDTH, PLAYER_HEIGHT))
+			img_left = pygame.transform.flip(img_right, True, False)
+			self.images_right.append(img_right)
+			self.images_left.append(img_left)
+
+
+		img = pygame.image.load('img/guy1.png')
+		self.image = pygame.transform.scale(img, (PLAYER_WIDTH, PLAYER_HEIGHT))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+		self.direction = 0
+
+	def update(self):
+		dx = 0
+		dy = 0
+		walk_cooldown = 5
+
+		# get keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_SPACE] and self.jumped == False:
+			self.vel_y -= 15
+			self.jumped = True
+		if not key[pygame.K_SPACE]:
+			self.jumped = False
+		if key[pygame.K_RIGHT]:
+			dx += 5
+			self.counter += 1
+			self.direction = 1
+		if key[pygame.K_LEFT]:
+			dx -= 5
+			self.counter += 1
+			self.direction = -1
+		if not key[pygame.K_LEFT] and not key[pygame.K_RIGHT]:
+			self.counter = 0
+			self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			elif self.direction == -1:
+				self.image = self.images_left[self.index]
+
+
+		# handle animation
+		if self.counter > walk_cooldown:
+			self.counter = 0
+			self.index += 1
+			if self.index >= len(self.images_right):
+				self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			elif self.direction == -1:
+				self.image = self.images_left[self.index]
+
+		# add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		# check for collision
+
+		# update player coordinates
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > SCREEN_HEIGHT:
+			self.rect.bottom = SCREEN_HEIGHT
+
+
+		# draw player on screen
+		screen.blit(self.image, self.rect)
 
 class World():
 	def __init__(self, data):
@@ -52,6 +139,7 @@ class World():
 		for tile in self.tile_list:
 			screen.blit(tile[0], tile[1])
 
+
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
@@ -75,16 +163,20 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
+player = Player(100, SCREEN_WIDTH - PLAYER_HEIGHT - TILE_SIZE)
 world = World(world_data)
 
 run = True
 
 while run:
 
+	clock.tick(FPS)
+
 	screen.blit(BG_IMG, (0, 0))
 	screen.blit(SUN_IMG, (100, 100))
 
 	world.draw()
+	player.update()
 
 	draw_grid()
 
